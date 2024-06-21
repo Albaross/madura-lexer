1614d19fd069d9630823c5ebb2a9236378791ad5
manuel.feilen <manuel.feilen@de.rhenus.com>
2024-06-21 15:04:04 +0200

    refactoring

--- src/main/kotlin/org/maduralang/lexer/Lexer.kt
+++ src/main/kotlin/org/maduralang/lexer/Lexer.kt
@@ -16,41 +16,37 @@ class Lexer {
 
     private fun createToken(input: String, pos: Int): Token {
         val c = input[pos]
-        when (c) {
-            ' ', '\t', in '\n'..'\r' ->
-                return Whitespace("$c")
 
-            in 'a'..'z', in 'A'..'Z' ->
-                return consumeNameOrKeyword(input, pos)
+        if (c.isWhitespace())
+            return Whitespace("$c")
 
-            in '0'..'9' ->
-                return consumeNumber(input, pos)
+        if (c.isLetter())
+            return consumeNameOrKeyword(input, pos)
 
-            in '!'..'/', in ':'..'@', in '['..'`', in '{'..'~' ->
-                return consumeSymbol(input, pos)
+        if (c.isDigit())
+            return consumeNumber(input, pos)
 
-            else ->
-                return Invalid("$c")
-        }
+        if (c.isSymbol())
+            return consumeSymbol(input, pos)
+
+        return Invalid("$c")
     }
 
-    private fun consumeNameOrKeyword(input: String, pos: Int) = Name(consume(input, pos, ::isWordCharacter))
+    private fun consumeNameOrKeyword(input: String, pos: Int): Token {
+        return Name(consume(input, pos) { it.isWordCharacter() })
+    }
 
-    private fun consumeNumber(input: String, pos: Int) =
-        Number(consume(input, pos) { it in '0'..'9' })
+    private fun consumeNumber(input: String, pos: Int): Token {
+        return Number(consume(input, pos) { it.isDigit() })
+    }
 
-    private fun consumeSymbol(input: String, pos: Int) =
-        Symbol(consume(input, pos)
-        { it in '!'..'/' || it in ':'..'@' || it in '['..'`' || it in '{'..'~' })
+    private fun consumeSymbol(input: String, pos: Int): Token {
+        return Symbol(consume(input, pos) { it.isSymbol() })
+    }
 
     private fun consume(input: String, start: Int, predicate: (Char) -> Boolean): String {
         var pos = start + 1
         while (pos < input.length && predicate(input[pos])) pos += 1
         return input.substring(start, pos)
-    }
-
-    private fun isWordCharacter(c: Char): Boolean = when (c) {
-        in 'a'..'z', in 'A'..'Z', in '0'..'9', '_' -> true
-        else -> false
     }
 }
